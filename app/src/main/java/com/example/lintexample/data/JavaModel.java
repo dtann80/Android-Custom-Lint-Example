f6bea17016db524389ed2aec64274cebf02fce68
Dan Tann <dan.tann@buzzfeed.com>
2018-01-03 12:52:13 -0600

    Example of Android Lint

---
+++ app/src/main/java/com/example/lintexample/data/JavaModel.java
@@ -0,0 +1,10 @@
+package com.example.lintexample.data;
+
+public class JavaModel {
+    String TAG = "_lint_";
+
+
+    String getText() {
+        return "`lint`";
+    }
+}
---
+++ app/src/main/java/com/example/lintexample/data/Model.kt
@@ -0,0 +1,8 @@
+package com.example.lintexample.data
+
+class Model {
+
+    companion object {
+        val TAG = "lint"
+    }
+}
